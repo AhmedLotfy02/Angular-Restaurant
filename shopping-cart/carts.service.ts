243b6e0906b4cd91c08ce3e97c0b26231c2cc78b
Rowan Omar <61714846+Rowan-Omar@users.noreply.github.com>
2021-08-13 16:20:11 +0200

    Add files via upload

---
+++ shopping-cart/carts.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Product } from '../products/product';
+import { ProductsService } from '../products/products.service';
+import { Cart } from './cart';
+import { CARTS } from './mock-cart';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class CartsService {
+
+  //private carts: Cart[] = [];
+  private products: Product[] = [];
+  private price = 0;
+  constructor(private productsService: ProductsService) { }
+
+  getProducts(){
+    return this.products;
+  }
+
+  addToCart(product: Product){
+    this.products.push(product);
+    this.price += product.price;
+  }
+  getTotalPrice(){
+    console.log(this.price);
+    return this.price;
+
+  }
+}
---
+++ shopping-cart/mock-cart.ts
@@ -0,0 +1,11 @@
+import { Cart } from "./cart";
+
+export const CARTS: Cart[] =[
+  {prodName:"Crepe",
+  prodPrice:3.5,
+  prodCount:3},
+
+  {prodName:"Waffel",
+  prodPrice:5,
+  prodCount:2}
+]
