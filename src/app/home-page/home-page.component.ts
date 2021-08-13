cb83a438eb2f70269e840db7ec483cd23605c0b4
Rowan Omar <61714846+Rowan-Omar@users.noreply.github.com>
2021-08-13 17:49:54 +0200

    comments added

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
-
+  //comment 34an n4of ht3dl hnak w y7slo update wla l2
+  //5las kda a3ml commit and push to origin w kda yb2a tmam ..a4t8ly 3la angular restaurant m4 ng restaurant
+  //mogrd ma a3ml save hy3rf
   constructor() { }
 
   ngOnInit(): void {
