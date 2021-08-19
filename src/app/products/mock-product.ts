2abf465a22b12937d4000a1e516275adf6076010
AhmedLotfy02 <76037906+AhmedLotfy02@users.noreply.github.com>
2021-08-20 01:43:45 +0200

    Updates
    PopUpForm is Updated
    we need property for each product which is fav to bind between the fav icon in card and that in popupform..easy we will do it later ,
    we want to remmber about link in popup form واجبات وافل as an example don't forget to run this command
    npm install material-design-icons-iconfont
    for more icon shapes

--- src/app/products/mock-product.ts
+++ src/app/products/mock-product.ts
@@ -1,8 +1,36 @@
-import { Product } from "./product";
+import { Product } from './product';
 
-export const PRODUCTS: Product[] =[
-  {id:"1", title:"كريب نوتيلا",description:"none", cover:"7921612441926_lg.jpg", price:3.5, counterCart:0},
-  {id:"2", title:"كريب بنانا",description:"none", cover:"6951612286608_lg.jpg", price:4.5, counterCart:0},
-  {id:"3", title:"كريب فواكة",description:"none", cover:"1381612287112_lg.jpg", price:5, counterCart:0},
-  {id:"4", title:"كريب بستاشيو",description:"none", cover:"8131612287722_lg.jpg", price:6.5, counterCart:0}
-]
+export const PRODUCTS: Product[] = [
+  {
+    id: '1',
+    title: 'كريب نوتيلا',
+    description: 'مزيج خورافي بين النوتيلا والبنانا',
+    cover: '7921612441926_lg.jpg',
+    price: 3.5,
+    counterCart: 0,
+  },
+  {
+    id: '2',
+    title: 'كريب بنانا',
+    description: 'مزيج خورافي بين النوتيلا والبنانا',
+    cover: '6951612286608_lg.jpg',
+    price: 4.5,
+    counterCart: 0,
+  },
+  {
+    id: '3',
+    title: 'كريب فواكة',
+    description: 'مزيج خورافي بين النوتيلا والبنانا',
+    cover: '1381612287112_lg.jpg',
+    price: 5,
+    counterCart: 0,
+  },
+  {
+    id: '4',
+    title: 'كريب بستاشيو',
+    description: 'مزيج خورافي بين النوتيلا والبنانا',
+    cover: '8131612287722_lg.jpg',
+    price: 6.5,
+    counterCart: 0,
+  },
+];
