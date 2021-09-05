fc859489dd63c7a283d219ba4e0afa8173e128d6
AhmedLotfy02 <76037906+AhmedLotfy02@users.noreply.github.com>
2021-09-05 20:05:44 +0200

    Updates
    IDs are updated and left side nav of cart is updated

--- src/app/products/mock-product.ts
+++ src/app/products/mock-product.ts
@@ -2,7 +2,7 @@ import { Product } from './product';
 
 export const crepeProd: Product[] = [
   {
-    id: '2',
+    id: '1',
     title: 'كريب بنانا',
     description: 'لعشاق الفراولة والموز هذا الطبق مناسب',
     cover: '6951612286608_lg.jpg',
@@ -10,7 +10,7 @@ export const crepeProd: Product[] = [
     counterCart: 0,
   },
   {
-    id: '3',
+    id: '2',
     title: 'كريب فواكة',
     description: 'مزيج خورافي بين النوتيلا والبنانا',
     cover: '1381612287112_lg.jpg',
@@ -18,7 +18,7 @@ export const crepeProd: Product[] = [
     counterCart: 0,
   },
   {
-    id: '4',
+    id: '3',
     title: 'كريب نوتيلا',
     description: 'كريب مثلث الشكل محشي نوتيلا ومغطي من الخارج بالنوتيلا',
     cover: '7921612441926_lg.jpg',
@@ -26,7 +26,7 @@ export const crepeProd: Product[] = [
     counterCart: 0,
   },
   {
-    id: '5',
+    id: '4',
     title: 'كريب بستاشيو',
     description: 'صلصة الفستق الحلبي ( رائعة رائعة رائعة )',
     cover: '8131612287722_lg.jpg',
@@ -34,7 +34,7 @@ export const crepeProd: Product[] = [
     counterCart: 0,
   },
   {
-    id: '6',
+    id: '5',
     title: 'كريب لوتس',
     description: 'زبدة اللوتس الرائعة',
     cover: '8631612295581_lg.jpg',
@@ -45,7 +45,7 @@ export const crepeProd: Product[] = [
 
 export const waffelProd: Product[] = [
   {
-    id: '1',
+    id: '6',
     title: 'وافل نوتيلا',
     description: 'وافل دائري يقدم مع النوتيلا',
     cover: '7021612447290_md.jpg',
@@ -53,7 +53,7 @@ export const waffelProd: Product[] = [
     counterCart: 0,
   },
   {
-    id: '2',
+    id: '7',
     title: 'وافل لوتس',
     description: 'وافل بزبدة اللوتس',
     cover: '361612447435_md.jpg',
@@ -64,7 +64,7 @@ export const waffelProd: Product[] = [
 
 export const tsunamiProd: Product[] = [
   {
-    id: '1',
+    id: '8',
     title: 'تسونامي فليك    ',
     description: 'نستلة فليك ، نوتيلا وايت ،',
     cover: '3421612446335_md.jpg',
@@ -72,7 +72,7 @@ export const tsunamiProd: Product[] = [
     counterCart: 0,
   },
   {
-    id: '2',
+    id: '9',
     title: 'تسونامي نوتيلا',
     description: 'الطعم رهيب',
     cover: '1571612446465_md.jpg',
@@ -83,7 +83,7 @@ export const tsunamiProd: Product[] = [
 
 export const lokaymatProd: Product[] = [
   {
-    id: '1',
+    id: '10',
     title: 'لقيمات نوتيلا',
     description: 'لقيمات تركية تقدم مع النوتيلا ،',
     cover: '1031612443211_md.jpg',
@@ -91,7 +91,7 @@ export const lokaymatProd: Product[] = [
     counterCart: 0,
   },
   {
-    id: '2',
+    id: '11',
     title: 'لقيمات لوتس    ',
     description: 'لقيمات تركية تقدم مع بسكويت وزبدة اللوتس',
     cover: '891612443246_md.jpg',
@@ -102,7 +102,7 @@ export const lokaymatProd: Product[] = [
 
 export const pancakeProd: Product[] = [
   {
-    id: '1',
+    id: '12',
     title: 'بان كيك نوتيلا    ',
     description: '٣ طبقات مع النوتيلا ،',
     cover: '9031612444619_md.jpg',
@@ -110,7 +110,7 @@ export const pancakeProd: Product[] = [
     counterCart: 0,
   },
   {
-    id: '2',
+    id: '13',
     title: 'بان كيك فواكه    ',
     description: 'بان كيك يقدم مع الفواكه ',
     cover: '8571612444697_md.jpg',
@@ -121,7 +121,7 @@ export const pancakeProd: Product[] = [
 
 export const mo3gnatProd: Product[] = [
   {
-    id: '1',
+    id: '14',
     title: 'قطعة دونات    ',
     description: ' قطعة دونات بالصلصة الي تحبوها    ',
     cover: '8921612446551_md.jpg',
@@ -129,7 +129,7 @@ export const mo3gnatProd: Product[] = [
     counterCart: 0,
   },
   {
-    id: '2',
+    id: '15',
     title: 'قطعتين دونات    ',
     description: 'قطعتان دونات بالصلصة الي تحبوها ',
     cover: '5571612446616_md.jpg',
@@ -140,7 +140,7 @@ export const mo3gnatProd: Product[] = [
 
 export const eidProd: Product[] = [
   {
-    id: '1',
+    id: '16',
     title: 'بان كيك سبشل    ',
     description: ' ( موز ، براونيز ، كريمة ، نوعين شوكولا )',
     cover: '3641620765049_md.jpg',
@@ -148,7 +148,7 @@ export const eidProd: Product[] = [
     counterCart: 0,
   },
   {
-    id: '2',
+    id: '17',
     title: 'سبشل كريب لوتس    ',
     description: '( موز ، كريمة ، براونيز ، لوتس )     ',
     cover: '3071620765124_md.jpg',
@@ -159,7 +159,7 @@ export const eidProd: Product[] = [
 
 export const PRODUCTS: Product[] = [
   {
-    id: '1',
+    id: '18',
     title: 'كريب نوتيلا',
     description: 'مزيج خورافي بين النوتيلا والبنانا',
     cover: '7921612441926_lg.jpg',
@@ -167,7 +167,7 @@ export const PRODUCTS: Product[] = [
     counterCart: 0,
   },
   {
-    id: '2',
+    id: '19',
     title: 'كريب بنانا',
     description: 'مزيج خورافي بين النوتيلا والبنانا',
     cover: '6951612286608_lg.jpg',
@@ -175,7 +175,7 @@ export const PRODUCTS: Product[] = [
     counterCart: 0,
   },
   {
-    id: '3',
+    id: '20',
     title: 'كريب فواكة',
     description: 'مزيج خورافي بين النوتيلا والبنانا',
     cover: '1381612287112_lg.jpg',
@@ -183,7 +183,7 @@ export const PRODUCTS: Product[] = [
     counterCart: 0,
   },
   {
-    id: '4',
+    id: '21',
     title: 'كريب بستاشيو',
     description: 'مزيج خورافي بين النوتيلا والبنانا',
     cover: '8131612287722_lg.jpg',
